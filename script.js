f667db12ff70682bd620f952b5bdabf02703cf3b
matun02 <91181156+matun02@users.noreply.github.com>
2026-04-06 08:28:34 +0900

    chore: remove unused app.js and bump version to 0.4

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const GOOGLE_CONFIG = {
   SCOPE: 'https://www.googleapis.com/auth/drive.file',
 };
 
-const APP_VERSION = '0.3';
+const APP_VERSION = '0.4';
 const APP_VERSION_STORAGE_KEY = 'kakeibo_app_version';
 
 const STORAGE_KEYS = {
